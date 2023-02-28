28e9ed86150e9d7b069cdce57c0992e284785c6b
fredrikadr <fredrikadr@gmail.com>
2023-02-28 23:24:11 +0100

    add discard function to api

--- src/pages/api/add/[...discard].ts
+++ src/pages/api/add/[...discard].ts
@@ -5,14 +5,14 @@ type Data = {
   response: JSON
 }
 
-export default async function getDeck(
+export default async function addToDiscardPile(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req.query.discard) {
     try {
-      const deckid = req.query.discard[0]
-      const card = req.query.discard[1]
+      const deckid = req.query.discard[0];
+      const card = req.query.discard[1];
       const response = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckid}/pile/discard/add/?cards=${card}`);
       const deck = await response.json();
       res.status(200).json(deck);
