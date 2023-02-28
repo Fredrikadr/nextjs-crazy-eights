875302fe9c36dbbfe8fbb8e121008641f30e3346
fredrikadr <fredrikadr@gmail.com>
2023-02-28 14:19:39 +0100

    add discard api route

---
+++ src/pages/api/add/[...discard].ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+type Data = {
+  response: JSON
+}
+
+export default async function getDeck(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.query.discard) {
+    try {
+      const deckid = req.query.discard[0]
+      const card = req.query.discard[1]
+      const response = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckid}/pile/discard/add/?cards=${card}`);
+      const deck = await response.json();
+      res.status(200).json(deck);
+    } catch (error: any) {
+      res.status(400).json({ response: error.message })
+    }
+  }
+
+}
+
+ 
